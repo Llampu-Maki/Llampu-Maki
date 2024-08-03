affa24522cb164eaef2ae0569b115ab586a06bd6
Llampu-Maki <lyqluque@gmail.com>
2024-08-03 16:24:40 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Gracias por contactarnos. Pronto nos pondremos en contacto contigo.');
+});
